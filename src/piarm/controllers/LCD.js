74eb32672d7a514bf1d1a7f53fad1c8537df1f6b
Daniel Robinson <d7robin@gmail.com>
2015-07-03 23:43:41 +0200

    converted LCD.js to ecma 6

--- src/piarm/controllers/LCD.js
+++ src/piarm/controllers/LCD.js
@@ -1,22 +1,41 @@
 /**
  * Created by Daniel on 2015-07-03.
  */
-var Lcd = require('lcd'),
-    lcd = new Lcd({rs: 12, e: 16, data: [6, 13, 19, 26], cols: 20, rows: 2});
+import Lcd from 'lcd'
 
-//       BOARD  BCM
-// RS -- 32 --- 12
-// E  -- 36 --- 16
-// D4 -- 31 --- 6
-// D5 -- 33 --- 13
-// D6 -- 35 --- 19
-// D7 -- 37 --- 26
+class LCD {
 
-// Here is a test function for the lcd..
+    //       BOARD  BCM
+    // RS -- 32 --- 12
+    // E  -- 36 --- 16
+    // D4 -- 31 --- 6
+    // D5 -- 33 --- 13
+    // D6 -- 35 --- 19
+    // D7 -- 37 --- 26
 
-lcd.on('ready', function () {
-    setInterval(function () {
-        lcd.setCursor(0, 0);
-        lcd.print(new Date().toISOString().substring(11, 19));
-    }, 1000);
-});
+
+
+    constructor()
+    {
+        this.lcd = new Lcd({rs: 12, e: 16, data: [6, 13, 19, 26], cols: 20, rows: 2});
+
+        // Here is a test function for the lcd.. It displays the time
+        this.lcd.on('ready', function () {
+            setInterval(function () {
+                this.lcd.setCursor(0, 0);
+                this.lcd.print(new Date().toISOString().substring(11, 19));
+            }.bind(this), 1000);
+        }.bind(this));
+
+
+        // If ctrl+c is hit, free resources and exit.
+        process.on('SIGINT', function () {
+            this.lcd.close();
+            process.exit();
+        }.bind(this));
+    }
+
+
+}
+const run = new LCD();
+export default run;
