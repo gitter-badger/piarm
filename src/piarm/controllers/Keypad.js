ce358d2a58ca042444ea780b28eed5c9d2ef912b
Julien Vincent <julienlucvincent@gmail.com>
2015-07-05 19:04:01 +0200

    added new tables, converted callbacks to pure es6

--- src/piarm/controllers/Keypad.js
+++ src/piarm/controllers/Keypad.js
@@ -3,11 +3,9 @@ import keypress from 'keypress'
 class Keypad {
     constructor(){
         keypress(process.stdin);
-        // well done Julien
         process.stdin.setRawMode(true);
-        process.stdin.resume();
 
-        process.stdin.on('keypress', function (ch, key) {
+        process.stdin.on('keypress', (ch, key) => {
             console.log('got "keypress"', key, ch);
             if (typeof key !== "undefined") {
                 if (key.ctrl && key.name == 'c') {
@@ -16,7 +14,7 @@ class Keypad {
                     process.exit(0);
                 }
             }
-        }.bind(this));
+        });
     }
 }
 const run = new Keypad();
