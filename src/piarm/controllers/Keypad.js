3500a831192db6881610bb8f85164f5755a8802d
Daniel Robinson <d7robin@gmail.com>
2015-07-05 12:46:43 +0200

    well done Julien for finding a way to capture raw characters from stdin

--- src/piarm/controllers/Keypad.js
+++ src/piarm/controllers/Keypad.js
@@ -1,25 +1,3 @@
-/**
- * Created by Daniel on 2015-07-05.
- */
-
-var keydown = require('keydown')
-
-var kd = keydown(['<control>', 'a'])
-
-kd.on('pressed', function() {
-    // control + a are both pressed right now
-    console.log("control + a are both pressed right now")
-})
-
-/*var Listener = require('../lib/keypress')
-
-var listener = new window.keypress.Listener();
-
-listener.simple_combo("shift s", function() {
-    console.log("You pressed shift and s");
-});*/
-
-/*
 var keypress = require('keypress');
 
 // make `process.stdin` begin emitting "keypress" events
@@ -36,4 +14,6 @@ process.stdin.on('keypress', function (ch, key) {
 });
 
 process.stdin.resume();
-*/
+
+// well done Julien
+process.stdin.setRawMode(true);
